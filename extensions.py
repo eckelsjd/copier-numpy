11fa56e2c0380c673ab68c6a72a8d00f654c00d7
Joshua Eckels <eckelsjd@umich.edu>
2026-03-04 16:13:00 -0500

    working on uv

--- extensions.py
+++ extensions.py
@@ -3,10 +3,8 @@ import shutil
 import subprocess
 import unicodedata
 from datetime import date
-from pathlib import Path
-import os
 import urllib.request
-import urllib
+import urllib.error
 
 from jinja2.ext import Extension
 
@@ -39,23 +37,6 @@ def slugify(value, separator="-"):
     return re.sub(r"[-_\s]+", separator, value).strip("-_")
 
 
-def pypi_name_exists(package_name: str) -> bool:
-    """Check if a package name exists in PyPI."""
-    url = f'https://pypi.org/project/{package_name}'
-    try:
-        urllib.request.urlopen(url)
-        return True
-    except urllib.error.HTTPError as e:
-        if e.code == 404:
-            return False
-        else:
-            raise
-
-
-def path_exists(path: str) -> bool:
-    return Path(path).exists()
-
-
 def format_python_version(version: str) -> str:
     """Just return the first x.x version number of a Python PEP508 version specifier"""
     first_version = version.split(",")[0]
@@ -69,6 +50,11 @@ def format_python_version(version: str) -> str:
             f"{'+' if '>' in first_version else ''}")
 
 
+def manager_exists(manager_name: str) -> bool:
+    """Check if a Python project manager is installed."""
+    return shutil.which(manager_name) is not None
+
+
 class TemplateDefaultExtension(Extension):
     """Provide some default answers as global variables to speed up template initialization"""
     def __init__(self, environment):
@@ -76,12 +62,9 @@ class TemplateDefaultExtension(Extension):
         email = git_user_email('')
         environment.globals["git_user_name"] = git_user_name('')
         environment.globals["git_user_email"] = email
-        environment.globals["git_username"] = email.split('@')[0]
+        environment.globals["github_user"] = email.split('@')[0]
         environment.globals["current_year"] = date.today().year
-        environment.globals["project_dir"] = Path(os.getcwd()).resolve().name
-        environment.globals["is_copier_update"] = path_exists(".copier-answers.yml") or path_exists(".copier-answers.yaml")
-        # runs *before* update/copy, so answers.yml will only exist when we are doing copier update
 
-        environment.filters["pypi_name_exists"] = pypi_name_exists
         environment.filters["slugify"] = slugify
         environment.filters["format_python_version"] = format_python_version
+        environment.filters["manager_exists"] = manager_exists
